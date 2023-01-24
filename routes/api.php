43d9e77b70a1d31ddf6d025fcc30622cb55fc63f
Aroa <93058586+Aroagn@users.noreply.github.com>
2023-01-24 18:53:54 +0100

    Practica Laravel 1

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentsController;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('/students')->controller(StudentsController::class)->group(function () {
+Route::prefix('/students')->controller(StudentController::class)->group(function () {
     Route::get('', 'getAll');
     Route::middleware('validate.id')->get('{id}', 'getById');
     Route::post('', 'create');
